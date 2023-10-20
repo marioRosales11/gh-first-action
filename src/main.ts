2f018f71a5f6bbfc32914c6ad849f498805f6438
marioRosales11 <mario.rosales@helloiconic.com>
2023-10-20 11:44:03 -0600

    deploying in 3 2 1

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3002);
+  console.log('Hello from cluster =D')
 }
 bootstrap();
